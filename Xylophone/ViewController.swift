7e5f3cf045a29c8d32534deb78eef929b8769b40
Sharandeep Singh <demo@demo.demo>
2023-04-28 13:07:31 +0530

    Added fading in and fading out functionality to buttons

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -6,6 +6,12 @@ class ViewController: UIViewController {
 
     @IBAction func keyPressed(_ sender: UIButton) {
         if let buttonLabel = sender.currentTitle {
+            sender.alpha = 0.5
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                  sender.alpha = 1.0
+              }
+            
             playSound(soundName: buttonLabel)
         }
     }
